8e8d4ce36888f5acdf1e6e69a3707d429d3e54e5
Anmol Verma <anmol.verma@mutualmobile.com>
2021-03-20 20:58:39 +0530

    add: added more protos for Contacts.proto and Chat.proto

--- src/main/kotlin/me/anmolverma/MainGrpcServer.kt
+++ src/main/kotlin/me/anmolverma/MainGrpcServer.kt
@@ -2,15 +2,21 @@ package me.anmolverma
 
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
 import me.anmolverma.users.services.UsersService
+import java.io.File
 
 object MainGrpcServer {
 
     @JvmStatic
-    fun main() {
+    fun main(args: Array<String>) {
         val server = NettyServerBuilder.forPort(8443)
+            .useTransportSecurity(
+                // reference file
+                File("./src/ssl/server.crt"),
+                File("./src/ssl/server.pem")
+            )
             .addService(UsersService()).build()
         server.start()
-
+        print("GRPC server on port ${server.port} ,started!")
         server.awaitTermination()
     }
 
